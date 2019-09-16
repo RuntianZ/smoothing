b3f642a91219a93873189b9fe17e9a84694c4b60
Runtian Zhai <runtianzhai@gmail.com>
2019-09-16 10:10:08 +0800

    Update datasets.py

--- code/datasets.py
+++ code/datasets.py
@@ -18,7 +18,7 @@ DATASETS = ["imagenet", "cifar10"]
 def get_dataset(dataset: str, split: str) -> Dataset:
     """Return the dataset as a PyTorch Dataset object"""
     if dataset == "imagenet":
-        return _imagenet(split)
+        return _imagenet_on_philly(split)
     elif dataset == "cifar10":
         return _cifar10(split)
 
